39dfb665249932a00f4b2f362a3c2414bcc3d7e5
Prosper <gndptb@gmail.com>
2022-05-09 04:52:08 -0700

    first intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,29 +2,31 @@ import "./intro.scss";
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
 
-export default function Intro() {
+export default function Intro ()
+{
   const textRef = useRef();
 
-  useEffect(() => {
-    init(textRef.current, {
+  useEffect( () =>
+  {
+    init( textRef.current, {
       showCursor: true,
       backDelay: 1500,
-      backSpeed:60,
-      strings: ["Developer", "Designer", "Content Creator"],
-    });
-  }, []);
+      backSpeed: 60,
+      strings: [ "Frontend Dev💻", "MERN-Stack Dev😎", "Mobile Dev📱" ],
+    } );
+  }, [] );
 
   return (
     <div className="intro" id="intro">
       <div className="left">
-        <div className="imgContainer">
-          <img src="assets/man.png" alt="" />
+        <div className="wrapper">
+          <div className="background"></div>
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
-          <h1>Prosper</h1>
+          <h1>HoluWatobi</h1>
           <h3>
             Freelance <span ref={textRef}></span>
           </h3>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,24 +1,30 @@
 import "./menu.scss";
 
-export default function Menu({ menuOpen, setMenuOpen }) {
+export default function Menu ( { menuOpen, setMenuOpen } )
+{
   return (
-    <div className={"menu "+(menuOpen && "active")}>
+    <div className={"menu " + ( menuOpen && "active" )}>
       <ul>
-        <li onClick={()=>setMenuOpen(false)}>
+        <li onClick={() => setMenuOpen( false )}>
           <a href="#intro">Home</a>
         </li>
-        <li onClick={()=>setMenuOpen(false)}>
+        <hr />
+        <li onClick={() => setMenuOpen( false )}>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li onClick={()=>setMenuOpen(false)}>
+        <hr />
+        <li onClick={() => setMenuOpen( false )}>
           <a href="#works">Works</a>
         </li>
-        <li onClick={()=>setMenuOpen(false)}>
+        <hr />
+        <li onClick={() => setMenuOpen( false )}>
           <a href="#testimonials">Testimonials</a>
         </li>
-        <li onClick={()=>setMenuOpen(false)}>
+        <hr />
+        <li onClick={() => setMenuOpen( false )}>
           <a href="#contact">Contact</a>
         </li>
+        <hr />
       </ul>
     </div>
   );
